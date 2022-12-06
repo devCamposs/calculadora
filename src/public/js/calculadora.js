76e9dae7dfa89fdcc67d5fa7b67d5d1299b73e1f
devCamposs <thomas.campos7@gmail.com>
2022-12-05 21:53:36 -0300

    arquivosComp

--- src/public/js/calculadora.js
+++ src/public/js/calculadora.js
@@ -1,3 +1,19 @@
 function insert(n1){
-document.getElementById('resultado').innerHTML = n1
-}
+  var numero = document.getElementById('resultado').innerHTML
+  document.getElementById('resultado').innerHTML = numero + n1
+  }
+  function clean() {
+  document.getElementById('resultado').innerHTML = ''
+  }
+  function back(){
+   var resultado = document.getElementById('resultado').innerHTML
+   document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+  }
+  function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML
+    if(resultado){
+      document.getElementById('resultado').innerHTML  = eval(resultado)
+    } else {
+
+    }
+  }
